c781014eafefbf36ffee4038560c0878bb621943
Erin Black <erinblackdesign@gmail.com>
2017-05-28 15:13:02 -0500

    got the math formula to work inside the server.js file

--- server.js
+++ server.js
@@ -17,17 +17,42 @@ app.listen(3000, function(){
 
 app.post('/calculation', function(req, res){
 console.log('post hit to /calculation and sent: ', req.body);
-  function executeCalculation(num1, num2, calcType){
+  var calc = executeCalculation(req.body.firstInput, req.body.secondInput, req.body.type);
+  //start the function to calculate the math
+  function executeCalculation(num1, num2, type){
     var parsedNum1 = parseInt(num1);
     var parsedNum2 = parseInt(num2);
-    var computeCalculation = parsedNum1 + parsedNum2;
+
+    var computeCalculation = finalCalc(parsedNum1, parsedNum2, type);
+    //final calculation function
+    function finalCalc(num1, num2, calcType){
+      console.log('finalCal calctype', calcType);
+      var type = calcType;
+      //switch statement to determine which calculation to make
+      switch(type){
+        case 'add':
+        computeCalculation = num1 + num2 ;
+        break;
+        case 'subtract':
+        computeCalculation = num1 - num2;
+        break;
+        case 'multiply':
+        computeCalculation = num1 * num2;
+        break;
+        case 'divide':
+        computeCalculation = num1 / num2;
+        break;
+      }// end switch statement
+      return computeCalculation;
+    }// end finalCalc function
+
     console.log(computeCalculation);
     return computeCalculation;
   }//end executeCalculation
-  var calc = executeCalculation(req.body.firstInput, req.body.secondInput, req.body.calcType);
+
   var result = new Object();
   result.answer = calc;
   //send the result
-  res.send(result);
+  res.send('' + result + '');
 
 }); //end app.post
