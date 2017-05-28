68c5a05b4e081f7fe42022f28fb0f592d02c0698
Erin Black <erinblackdesign@gmail.com>
2017-05-28 12:10:41 -0500

    Created the initial html framework

--- public/scripts/calculator.js
+++ public/scripts/calculator.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
   console.log('jquery sourced');
-});
+
+  $('.mathButtons').on('click', function(){
+    $.ajax({
+      type: 'POST',
+      url: '/calculation'
+    });//end ajax call
+  });//end onClick
+
+});//end document.ready
+
+
+//calcuation function
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+//requires
 var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 
+//uses
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+
+//listen
 app.listen(3000, function(){
   console.log('server up on 3000');
+});
+
+//post calculations
+
+app.post('/calculation', function(req, res){
+console.log('post hit to /calculation and sent: ', req.body);
 });
