7cf41ff7d8cd39a8298fd06bc5ae0b9cf75497a6
Erin Black <erinblackdesign@gmail.com>
2017-05-28 12:37:49 -0500

    completed the function to get the input and calculator info

--- public/scripts/calculator.js
+++ public/scripts/calculator.js
@@ -1,14 +1,24 @@
 $(document).ready(function(){
   console.log('jquery sourced');
 
-  $('.mathButtons').on('click', function(){
-    $.ajax({
-      type: 'POST',
-      url: '/calculation'
-    });//end ajax call
-  });//end onClick
+  $('#calculate').on('click', calculation );
+  // {
+  //   $.ajax({
+  //     type: 'POST',
+  //     url: '/calculation'
+  //   });//end ajax call
+  // });//end onClick
 
 });//end document.ready
 
 
-//calcuation function
+//calcuation object function
+function calculation (){
+  var calculation = new Object();
+  calculation.firstInput = $('#num1').val();
+  calculation.secondInput = $('#num2').val();
+  calculation.calcType = $('.mathButtons').attr('value');
+
+  console.log('calculation object is', calculation);
+
+}
